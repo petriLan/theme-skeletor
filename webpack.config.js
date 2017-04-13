406463b9db70ce5d6cab47452aa8f01ce82e4f47
Christian Nikkanen <hi@kisu.li>
2017-04-13 16:19:47 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+// const NyanProgressPlugin = require('nyan-progress-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
+const parts = require('./webpack.parts');
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+};
+
+const commonConfig = merge([
+  {
+    entry: {
+      app: PATHS.app,
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
+    },
+    plugins: [
+      new CaseSensitivePathsPlugin(), // complain about capitalisation
+      new NpmInstallPlugin(), // install dependencies automatically
+      new FriendlyErrorsWebpackPlugin(), // Webpack is mean by default,
+      // new NyanProgressPlugin(), // Show progress
+      new DashboardPlugin(),
+      new webpack.LoaderOptionsPlugin({
+        options: {
+          eslint: {
+            // Fail only on errors
+            failOnWarning: false,
+            failOnError: true,
+
+            // Toggle autofix
+            fix: true,
+
+            // Output to Jenkins compatible XML
+            // outputReport: {
+              // filePath: 'checkstyle.xml',
+              // formatter: require('eslint/lib/formatters/checkstyle'),
+            // },
+          },
+        },
+      }),
+
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo',
+      }),
+    ],
+  },
+  parts.lintJavaScript({ include: PATHS.app }),
+]);
+
+const productionConfig = merge([
+]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
+
+module.exports = (env) => {
+  if (env === 'production') {
+    return merge(commonConfig, productionConfig);
+  }
+
+  return merge(commonConfig, developmentConfig);
+};
---
+++ webpack.parts.js
@@ -0,0 +1,48 @@
+exports.devServer = ({ host, port } = {}) => ({
+  devServer: {
+    // Enable history API fallback so HTML5 History API based
+    // routing works. Good for complex setups.
+    historyApiFallback: true,
+
+    // Display only errors to reduce the amount of output.
+    stats: 'errors-only',
+
+    // Parse host and port from env to allow customization.
+    //
+    // If you use Docker, Vagrant or Cloud9, set
+    // host: options.host || '0.0.0.0';
+    //
+    // 0.0.0.0 is available to all network devices
+    // unlike default `localhost`.
+    host: process.env.HOST, // Defaults to `localhost`
+    port: process.env.PORT, // Defaults to 8080
+
+    overlay: {
+      errors: true,
+      warnings: true
+    },
+
+    // proxy: {
+    // '/./': {
+    // target: 'https://pro-tukipiste.local',
+    // secure: false,
+    // },
+    // },
+  },
+});
+
+exports.lintJavaScript = ({ include, exclude, options }) => ({
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true,
+        },
+      },
+    ],
+  },
+});
