3b18d38b846eace3a5debb3ee94272ad129d88f5
Christian Nikkanen <me@kisu.li>
2017-04-27 13:16:11 +0300

    Use key-value pairs instead of values

--- includes/polylang.php
+++ includes/polylang.php
@@ -3,17 +3,17 @@ if (function_exists('pll_register_string')) {
   // Remember, it's the second parameter that you use for pll_e
   $strings = [
     // 'context_string', 'Use this value in pll__',
-    'Skip to content button', 'Skip to content',
-    'Log in', 'Log in',
-    'Log out', 'Log out',
-    'Read more', 'Read more',
-    'Email', 'Email',
-    'Password', 'Password',
-    'Back', 'Back',
-    'Search', 'Search',
-    'Instruct user to enter search terms', 'Enter search term',
-    'Inform user about no results from search', 'No search results',
-    'News', 'News'
+    'Skip to content button' => 'Skip to content',
+    'Log in' => 'Log in',
+    'Log out' => 'Log out',
+    'Read more' => 'Read more',
+    'Email' => 'Email',
+    'Password' => 'Password',
+    'Back' => 'Back',
+    'Search' => 'Search',
+    'Instruct user to enter search term' => 'Enter search term',
+    'Inform user about no results from search' => 'No search results',
+    'News' => 'News'
   ];
 
   foreach ($strings as $ctx => $value) {
