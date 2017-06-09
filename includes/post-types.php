6d551a5ad64547c0e7cadd0391ce0f53cdc85db1
Christian Nikkanen <me@kisu.li>
2017-06-09 12:00:26 +0300

    Conform to CS

--- includes/post-types.php
+++ includes/post-types.php
@@ -2,21 +2,24 @@
 add_action('init', function() {
   add_post_type_support('page', 'excerpt');
 
-  /*register_post_type('sample_cpt', array(
-    'labels' => array(
-      'name' => pll__('Sample'),
-      'singular_name' => pll__('Sample singular')
-    ),
+  /*register_post_type('event', [
+    'labels' => [
+      'name' => pll__('Events'),
+      'singular_name' => pll__('Event'),
+    ],
     //'description' => __('description'),
     'public' => true,
-    'rewrite' => array('slug' => 'sample', 'with_front' => false),
+    'rewrite' => [
+      'slug' => 'event',
+      'with_front' => false,
+    ],
     'has_archive' => true,
     'exclude_from_search' => false,
     'publicly_queryable' => null,
     'show_ui' => true,
     'show_in_nav_menus' => null,
     'hierarchical' => false,
-    'supports' => array(
+    'supports' => [
       'title',
       'editor',
       //'comments',
@@ -27,16 +30,13 @@ add_action('init', function() {
       'page-attributes',
       'thumbnail',
       //'custom-fields'
-    ),
-    'taxonomies' => array(
+    ],
+    'taxonomies' => [
       //'post_tag',
       //'category',
-      'event_category'
-    ),
+      // 'event_category'
+    ],
     'capability_type' => 'post',
-    'capabilities' => array(
-      // for fine grained control include valid capabilities here
-      // if left empty 'capability_type' will define editing capability requirements
-    ),
-  ));*/
+  ]);*/
+
 });
