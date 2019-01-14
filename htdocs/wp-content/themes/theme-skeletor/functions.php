c11dbb6e94633fbc4b318a7c1638ebde55c9874f
Hannu Kärkkäinen <hannu@redandblue.fi>
2019-01-14 10:43:38 +0200

    Enable mail sending via SendGrid

--- htdocs/wp-content/themes/theme-skeletor/functions.php
+++ htdocs/wp-content/themes/theme-skeletor/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+define('SENDGRID_API_KEY', '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');
+
 foreach (glob(dirname(__FILE__) . "/includes/*") as $filename) {
   require_once($filename);
 }
