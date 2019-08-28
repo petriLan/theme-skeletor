2f4daaa268a9a345f0a1903b71b61154e1947fb8
Eemeli Makkonen <eemeli@redandblue.fi>
2019-08-28 14:12:59 +0300

    Some navigation + some nav walker

--- htdocs/wp-content/themes/theme-skeletor/footer.php
+++ htdocs/wp-content/themes/theme-skeletor/footer.php
@@ -1,4 +1,14 @@
   </main>
+  <footer>
+    <div class="container">
+      <div class="some-menu">
+        <?php wp_nav_menu([
+          'theme_location' => 'some',
+        ]);
+        ?>
+      </div>
+    </div>
+  </footer>
   <?php wp_footer(); ?>
   </body>
 </html>
